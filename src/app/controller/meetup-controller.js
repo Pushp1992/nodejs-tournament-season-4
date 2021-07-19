96cabfa0c68d5aeb2273cf3f1120b5a2dca9190e
Pushp Singh <pushp_singh@condenast.com>
2021-07-19 11:42:01 +0530

    boilerplate for online virtual meetup application

---
+++ src/app/controller/meetup-controller.js
@@ -0,0 +1,35 @@
+// All controller logic for CRUD operation will stay here
+
+const MeetupModel = require('../model/meetup-model');
+
+// Create and Save a new Member Profile
+exports.create = (req, res) => {
+    
+    // validate request body
+
+    // Creaate Member Profile
+
+    // Save Member Profile in Database
+ 
+};
+
+
+// Retrieve and return all Member Profile from the database.
+exports.findAll = (req, res) => {
+   // TODO from below
+};
+
+// Find a single Member Profile with a memberProfileId
+exports.findOne = (req, res) => {
+    // TODO from below
+};
+
+// Update a MemberProfile identified by the memberProfileId in the request
+exports.update = (req, res) => {
+    // TODO from below
+};
+
+// Delete a MemberProfile with the specified memberProfileId in the request
+exports.delete = (req, res) => {
+    // TODO from below
+};
---
+++ src/app/routes/index.js
@@ -0,0 +1,16 @@
+// all routes HTTP method (GET/PUT/POST/DELETE) will be present here
+
+module.exports = (app) => {
+    // import your existing Meetup controller here, which contains method for all CRUD operations
+
+    // APi endpoint to create a new Member profile
+
+    // API endpoint to get all Member profile
+
+    // API endpoint to get single Member profile
+
+    // API endpoint to update Member Profile with profileId
+
+    // API endpoint to delete Member Profile with profileId
+
+};
